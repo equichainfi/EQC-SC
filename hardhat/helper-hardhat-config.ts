b45e4ed00f2b1cb3be90bbcc084157aa497b483b
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-11-26 18:39:48 +0100

    EQC-71 fix

---
+++ hardhat/helper-hardhat-config.ts
@@ -0,0 +1,35 @@
+export const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
+    31337: {
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane:
+            "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    11155111: {
+        name: "sepolia",
+        subscriptionId: "588",
+        gasLane:
+            "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    1: {
+        name: "mainnet",
+        keepersUpdateInterval: "30",
+    },
+};
+
+export const developmentChains: string[] = ["hardhat", "localhost"];
+export const VERIFICATION_BLOCK_CONFIRMATIONS: number = 6;
+export const frontEndContractsFile: string =
+    "../equichain-webapp/src/lib/contracts/networkMapping.json";
+export const frontEndAbiLocations: string =
+    "../equichain-webapp/src/lib/contracts/abi";
