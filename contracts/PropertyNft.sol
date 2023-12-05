0a5995da88b465d1469acd157c05db3d44d44788
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-12-05 11:11:18 +0100

    EQC-75 simple nft near to end, but need to fix EIP-721 implementation

--- contracts/PropertyNft.sol
+++ contracts/PropertyNft.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.23;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import { ERC721Burnable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721Votes } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Votes.sol";
 
 error PropertyNft__NonexistentToken(uint256 tokenId);
 
@@ -11,23 +13,30 @@ error PropertyNft__NonexistentToken(uint256 tokenId);
 /// @author Olivier Kobialka, 2023
 /// @notice Contract for basic ERC-721 token simulation for marketplace
 
-contract PropertyNft is ERC721, ERC721Burnable, Ownable {
+//todo: fix EIP712 signature from unique
+contract PropertyNft is ERC721, EIP721, ERC721Burnable, Ownable, ERC721Votes {
     string public i_uri = "";
     uint256 private s_tokenCounter;
     address private initialOwner;
+    string private s_name;
+    string private s_symbol;
 
     event PropertyMinted(uint256 indexed tokenId, address indexed to);
 
     constructor(
-        address _initialOwner
-    ) ERC721("PropertyNft", "PNFT") Ownable(_initialOwner) {
+        address _initialOwner,
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) Ownable(_initialOwner) EIP712(_name, "1") {
         s_tokenCounter = 0;
         initialOwner = _initialOwner;
+        s_name = _name;
+        s_symbol = _symbol;
     }
 
-    function mint(uint256 tokeId, address to) public {
-        _safeMint(msg.sender, tokeId, "");
-        emit PropertyMinted(tokeId, to);
+    function mint(uint256 tokenId, address to) public {
+        _safeMint(msg.sender, tokenId, "");
+        emit PropertyMinted(tokenId, to);
         s_tokenCounter++;
     }
 
@@ -41,15 +50,31 @@ contract PropertyNft is ERC721, ERC721Burnable, Ownable {
         }
     }
 
-    function nftBurn(uint256 tokenId) public {
+    function nftBurn(uint256 tokenId) public returns (bool) {
         if (
             _requireOwned(tokenId) == initialOwner &&
             ownerOf(tokenId) != msg.sender
         ) revert PropertyNft__NonexistentToken(tokenId);
         _burn(tokenId);
+        return true;
     }
 
-    function getTokenCounter() public view returns (uint256) {
+    function getTokenCounter() public view returns (uint256 tokenCounter) {
         return s_tokenCounter;
+    }
+
+    function update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Votes) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Votes) {
+        super._increaseBalance(account, value);
     }
 }
