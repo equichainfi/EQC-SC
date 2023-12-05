9ee92103000df0c5b32d267642c9233959dadc30
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-12-05 20:29:00 +0100

    EQC-75 PT: fix override, and fix hh config + tsconfig.ts to run yarn run v1.22.19 $ C:\Users\olivi\OneDrive\Pulpit\equichain-webapp-blockend\node_modules\.bin\hardhat
    info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.

--- contracts/PropertyToken.sol
+++ contracts/PropertyToken.sol
@@ -1,4 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-contract PropertyToken {}
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { ERC20Pausable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import { AccessManaged } from "@openzeppelin/contracts/access/manager/AccessManaged.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { ERC20Votes } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import { Nonces } from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract PropertyToken is
+    ERC20,
+    ERC20Permit,
+    AccessManaged,
+    ERC20Burnable,
+    ERC20Votes,
+    ERC20Pausable
+{
+    uint256 public premint;
+
+    constructor(
+        uint256 _premint,
+        address initialAuthority,
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) ERC20Permit(name_) AccessManaged(initialAuthority) {
+        premint = _premint;
+        _mint(msg.sender, premint * 10 * decimals());
+    }
+
+    function pause() public restricted {
+        _pause();
+    }
+
+    function unpause() public restricted {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public restricted {
+        _mint(to, amount);
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
+    }
+
+    function nonces(
+        address owner
+    ) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
